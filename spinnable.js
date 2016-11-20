d2061e10bb5e6ac56ae5512cc250008a01d60538
James Quinlan <jquinlan@bricksimple.com>
2016-11-20 11:52:13 -0500

    Add function to run at end of lifecycle

--- spinnable.js
+++ spinnable.js
@@ -1,5 +1,6 @@
 class Spinnable {
     constructor(el, args) {
+        this.guid = this.createGUID();
         this.el = el;
         this.data = args.data;
         this.size = args.size || 200;
@@ -10,24 +11,27 @@ class Spinnable {
         this.rotation = -90;
         this.value = null;
         this.spinning = false;
-        this.guid = this.createGUID();
+        this.repeating = args.repeating;
+        this.alreadySelected = [];
+        this.availableData = [];
+        this.stroke = args.stroke;
+        this.borderSize = args.borderSize || 0;
+        this.borderColor = args.borderColor || 'transparent';
         this.afterSelected = args.afterSelected || function(){};
+        this.afterAllSelected = args.afterAllSelected || function(){};
+        this.trueSize = this.size + this.borderSize;
 
         this.init();
     }
 
     init() {
-        this.checkForRequired();
         this.renderSpinner();
         this.setElements();
         this.parseData();
+        this.filterAvailableData();
         this.renderSections();
         this.setEventListeners();
         this.setTransition();
-    }
-
-    checkForRequired() {
-        if(!this.data) throw `No data supplied for Spinnable instance: ${ this.guid }`;
     }
 
     createGUID() {
@@ -41,17 +45,16 @@ class Spinnable {
 
     renderSpinner() {
         this.el.innerHTML =
-            `<svg xmlns="http://www.w3.org/2000/svg" data-guid="${ this.guid }" style="display: block; margin: 0 auto;" version="1.1" width="${ this.size }" height="${ this.size }" viewBox="0 0 ${ this.size } ${ this.size }" preserveAspectRatio>
-                <circle cx="${ this.size / 2 }" cy="${ this.size / 2 }" r="${ this.size / 2 - 20}" fill="#222222"/>
-                <g transform="translate(${ this.size / 2 }, ${ this.size / 2 })">
-                    <g class="spinner-group" stroke="#000" stroke-width="0" style="transform: rotate(${ this.rotation }deg)">
+            `<svg xmlns="http://www.w3.org/2000/svg" data-guid="${ this.guid }" style="display: block; margin: 0 auto;" version="1.1" width="${ this.trueSize }" height="${ this.trueSize }" viewBox="0 0 ${ this.trueSize } ${ this.trueSize }" preserveAspectRatio>
+                <circle cx="${ this.trueSize / 2 }" cy="${ this.trueSize / 2 }" r="${ this.trueSize / 2 }" fill="${ this.borderColor }"/>
+                <g transform="translate(${ this.trueSize / 2 }, ${ this.trueSize / 2 })">
+                    <g class="spinner-group" style="transform: rotate(${ this.rotation }deg)">
 
                     </g>
                 </g>
                 <g id="spinner-board"></g>
-                <path id="spinner-arrow" d="M ${this.size/2-5} ${this.size/2} L ${this.size/2-5} 70 L ${this.size/2-12} 70 L ${this.size/2} 55 L ${this.size/2+12} 70 L ${this.size/2+5} 70 ${this.size/2+5} ${this.size/2} Z" fill="#EEEEEE" stroke="#222222" style="stroke-width:2px"/>
-                <circle cx="${ this.size / 2 }" cy="${ this.size / 2 }" r="18" fill="#444444" stroke="#222222" style="stroke-width:2px"/>
-                <circle cx="${ this.size / 2 }" cy="${ this.size / 2 }" r="9" fill="#666666" stroke="#222222" style="stroke-width:2px"/>
+                <path id="spinner-arrow" d="M ${this.trueSize/2-5} ${this.trueSize/2} L ${this.trueSize/2-5} ${ this.trueSize/4} L ${this.trueSize/2-12} ${ this.trueSize/4} L ${this.trueSize/2} ${this.trueSize/4-20} L ${this.trueSize/2+12} ${ this.trueSize/4} L ${this.trueSize/2+5} ${ this.trueSize/4} ${this.trueSize/2+5} ${this.trueSize/2} Z" fill="#EEEEEE" stroke="#222222" style="stroke-width:2px"/>
+                <circle cx="${ this.trueSize / 2 }" cy="${ this.trueSize / 2 }" r="18" fill="#444444" stroke="#222222" style="stroke-width:2px"/>
             </svg>`;
     }
 
@@ -60,6 +63,7 @@ class Spinnable {
     }
 
     renderSections() {
+        let svgNS = "http://www.w3.org/2000/svg";
         let sections = this.data;
         let sectionWidth = sections[0].endAngle - sections[0].startAngle;
 
@@ -67,8 +71,21 @@ class Spinnable {
             this.spinnerGroup.innerHTML =
                 this.spinnerGroup.innerHTML +
                 `<g transform="rotate(${  sectionWidth * i })">
-                    <path d="${ sections[i].d }" fill="${ sections[i].color }" stroke="#fff" stroke-width="1"/>
+                    <path d="${ sections[i].d }" fill="${ sections[i].color }" stroke="#fff" stroke-width="${ this.stroke || 0}"/>
                 </g>`;
+        }
+
+        if(this.text) {
+            for(let i = 0; i < this.spinnerGroup.children.length; i++){
+                let child = this.spinnerGroup.children[i];
+                let text = document.createElementNS(svgNS, 'text');
+                text.setAttribute('x', this.trueSize / 4);
+                text.setAttribute('y', this.trueSize / 4);
+                text.innerHTML = this.data[i].datum;
+                console.log(text);
+                child.appendChild(text)
+                console.log(child);
+            }
         }
     }
 
@@ -88,31 +105,46 @@ class Spinnable {
         x2 = parseInt(Math.round((this.size / 2) * Math.cos(Math.PI * (360 * percentOfWhole) / 180)))
         y2 = parseInt(Math.round((this.size / 2) * Math.sin(Math.PI * (360 * percentOfWhole) / 180)))
 
-        let d = 'M0,0  L' + x1 + ',' + y1 + `  A${ this.size / 2 },${ this.size / 2 } 0 ` +
+        let d = 'M0,0  L' + x1 + ',' + y1 + `  A${ this.trueSize / 2 },${ this.trueSize / 2 } 0 ` +
         (((360 * percentOfWhole) - 0 > 180) ? 1 : 0) + ',1 ' + x2 + ',' + y2 + ' z'
 
         return { datum, startAngle, endAngle, d, color: this.colors[index] }
       })
+
+      console.log(this.data);
     }
 
     rotateSpinner () {
+
       if(!this.spinning){
           this.spinning = true;
-          const randomDatum = this.data[Math.floor(Math.random() * this.data.length)];
-          console.log(randomDatum.color)
+          let datum = this.availableData[Math.floor(Math.random() * this.availableData.length)];
+          if(!this.repeating) this.updateSelected(datum)
           let diff = this.rotation % 360;
           let magnitude = Math.random() + 1;
-          this.rotation = this.rotation + ((this.numOfSpins * 360) + (360 - randomDatum.endAngle + Math.floor(Math.random() * ((randomDatum.endAngle - randomDatum.startAngle) / 2) * magnitude)) - 90 - diff);
+          this.rotation = this.rotation + ((this.numOfSpins * 360) + (360 - datum.endAngle + Math.floor(Math.random() * ((datum.endAngle - datum.startAngle) / 2) * magnitude)) - 90 - diff);
           this.spinnerGroup.style.transform = `rotate(${ this.rotation }deg)`;
 
           // Call the function to handle the selected piece of data.
           setTimeout(() => {
-              this.afterSelected(randomDatum);
+              this.afterSelected(datum);
+              if(!this.repeating && this.availableData.length === 0) this.afterAllSelected(datum);
           }, this.delay * 1000);
 
           // Set spinning bool to false so user can click again.
           setTimeout(() => { this.spinning = false }, this.delay * 1000)
       }
+    }
+
+    updateSelected(datum) {
+        this.alreadySelected.push(datum);
+        this.filterAvailableData();
+    }
+
+    filterAvailableData() {
+        this.availableData = this.data.filter(datum => {
+            return this.alreadySelected.indexOf(datum) === -1;
+        });
     }
 
     setEventListeners() {
